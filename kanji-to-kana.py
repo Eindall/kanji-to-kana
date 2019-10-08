2c1e913496057186c3a8de5d9047b405b6b3a351
Thomas Marquis <thomas.marquis@epitech.eu>
2019-10-08 18:55:17 +0200

    Set command_list to an embed

--- kanji-to-kana.py
+++ kanji-to-kana.py
@@ -2,11 +2,29 @@ import discord
 
 TOKEN = open("token", "r").read()
 
-prefix = 'k!'
+prefix = '&'
+
+command_list = discord.Embed(title="**__Command List__**", description="*Command's prefix : &*", color=0x00ff00)
+command_list.add_field(name="**help**", value="> Gives a list of every available commands", inline=False)
+command_list.add_field(name="**about**", value="> Gives infos about the bot and its creator", inline=False)
 
 class Client(discord.Client):
   async def on_ready(self):
     print("Logged on as ", self.user)
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(name="some weeb shit"))
+  async def on_message(self, message):
+    if message.author == self.user:
+      return
+    if message.content[0] == prefix:
+      args = message.content.split(' ')
+
+      if args[0] == prefix + "help":
+        await message.channel.send("<@" + str(message.author.id) + ">, here are the bot's commands : ")
+        await message.channel.send(embed=command_list)
+
+      if args[0] == prefix + "about":
+        await message.channel.send("> KanjiToKana is a bot created in order to help people that wants to learn Japanese. It has been created by Eindall#2121, feel free to add me if you have any question or would like to submit a feature request")
+
 
 client = Client()
 client.run(TOKEN)
