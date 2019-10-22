d3ea2ea812e7d3449e49577568a66be5424f5a81
Thomas Marquis <thomas.marquis@epitech.eu>
2019-10-22 14:19:30 +0200

    Added &kanji command, though it might need some changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kanji-to-kana
 A discord bot about japanese kanas and kanjis
+
+In order to launch the bot, please install discord.py and requests using pip
--- kanji-to-kana.py
+++ kanji-to-kana.py
@@ -1,11 +1,19 @@
 import discord
+import requests
+import os
 
 TOKEN = open("token", "r").read()
+
+API_KEY = open("api_key", "r").read()
 
 MAIN_COLOR = 0x673e74
 
 prefix = '&'
 
+api_headers = {
+  "x-rapidapi-host": "kanjialive-api.p.rapidapi.com",
+  "x-rapidapi-key": API_KEY
+}
 
 command_list = discord.Embed(title="**__Command List__**", description="*Command's prefix : &*", color=MAIN_COLOR)
 command_list.add_field(name="**help**", value="> Gives a list of every available commands", inline=False)
@@ -28,14 +36,41 @@ class Client(discord.Client):
     if message.content[0] == prefix:
       args = message.content.split(' ')
 
+      # Help command
       if args[0] == prefix + "help":
         await message.channel.send("<@" + str(message.author.id) + ">, here are the bot's commands : ")
         await message.channel.send(embed=command_list)
 
+      # About command
       if args[0] == prefix + "about":
         about.set_thumbnail(url=self.user.avatar_url)
         about.set_author(name="KanjiToKanaBot (Discord ID: {})".format(self.user.id), icon_url=self.user.avatar_url)
         await message.channel.send(embed=about)
+
+      # Kanji command
+      if args[0] == prefix + "kanji":
+        if len(args) < 2:
+          await message.channel.send("<@" + str(message.author.id) + "> No kanji given as argument.")
+        else:
+          request = requests.get('https://kanjialive-api.p.rapidapi.com/api/public/kanji/' + args[1], headers=api_headers)
+          request = request.json()
+          if 'error' in request:
+            await message.channel.send("I'm sorry <@" + str(message.author.id) + ">, but I couldn't find this kanji...")
+          else:
+            card = discord.Embed(title="Kanji Card - " + request['kanji']['character'], description=request['kanji']['meaning']['english'])
+            card.set_image(url="http://kanji.nihongo.cz/image.php?text=" + request['kanji']['character'] + "&font=sod.ttf&fontsize=300&color=white")
+            card.add_field(name="Pronounciation", value=request['kanji']['kunyomi']['hiragana'] + ' (' + request['kanji']['kunyomi']['romaji'] + ') | ' + request['kanji']['onyomi']['katakana'] + ' (' + request['kanji']['onyomi']['romaji'] + ')', inline=False)
+            if 'radical' in request:
+              card.add_field(name="Radical", value=request['radical']['character'] + " [" + request['radical']['name']['hiragana'] + " (" + request['radical']['name']['romaji'] + ")]")
+            await message.channel.send(embed=card)
+
+      # Test lol eussou
+      if args[0] == prefix + "test":
+        embed=discord.Embed(title="Kanji Card - 私", url="https://jisho.org/search/%E7%A7%81%20%23kanji", description="private, I, me")
+        embed.set_image(url="http://kanji.nihongo.cz/image.php?text=%E7%A7%81&font=sod.ttf&fontsize=300&color=white")
+        embed.add_field(name="Pronounciation", value="わたし (watashi) | し (SHI)", inline=False)
+        embed.add_field(name="Key(s)", value="厶 (28) | 禾 (115)", inline=False)
+        await message.channel.send(embed=embed)
 
 
 client = Client()
