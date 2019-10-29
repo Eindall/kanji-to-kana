4ec14c531274b001fc7c8d4fa6203e3c8efba6ea
Eindall <thomas.marquis@epitech.eu>
2019-10-29 12:24:36 +0100

    Reworked help command

--- commands/help.py
+++ commands/help.py
@@ -2,9 +2,24 @@ import globals as G
 import discord
 
 async def run(client, message, args):
-    command_list = discord.Embed(title="**__Command List__**", description="*Command's prefix : &*", color=G.MAIN_COLOR)
-    command_list.add_field(name="**help**", value="> Gives a list of every available commands", inline=False)
-    command_list.add_field(name="**about**", value="> Gives infos about the bot and its creator", inline=False)
-
-    await message.channel.send("<@" + str(message.author.id) + ">, here are the bot's commands : ")
-    await message.channel.send(embed=command_list)
+    if len(args) == 1:
+        for command in G.AVAILABLE_COMMANDS:
+            if args[0] == command["title"]:
+                matching_command = command
+                break
+        try:
+            matching_command
+        except NameError:
+            G.logger.warn("No command found. Arguments: {}".format(args))
+            return await message.channel.send("Sorry <@" + str(message.author.id) + ">, the command " + args[0] + " does not exist !")
+        else:
+            command_details = discord.Embed(title="**__Command details - " + matching_command["title"] + "__**", description=matching_command["short_desc"], color=G.MAIN_COLOR)
+            for usage in matching_command["usages"]:
+                command_details.add_field(name="**" + usage["title"] + "**", value="> " + usage["desc"]) 
+            await message.channel.send(embed=command_details)
+    else:
+        command_list = discord.Embed(title="**__Command List__**", description="*Command's prefix : &*", color=G.MAIN_COLOR)
+        for command in G.AVAILABLE_COMMANDS:
+            command_list.add_field(name="**" + command["title"] + "**", value="> " + command["short_desc"], inline=False) 
+        await message.channel.send("<@" + str(message.author.id) + ">, here are the bot's commands : ")
+        await message.channel.send(embed=command_list)
